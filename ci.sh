dda1ab3a56ae4521c7b5f287e0d15556b408d5cd
David Gonzalez Martin <davidgm94.work@protonmail.com>
2023-12-17 19:42:18 +0100

    Divide tests between standalone and integral

--- ci.sh
+++ ci.sh
@@ -6,24 +6,67 @@ zig build -Duse_llvm=$nativity_use_llvm
 failed_test_count=0
 passed_test_count=0
 test_directory_name=test
-test_directory=$test_directory_name/*
-total_test_count=$(ls 2>/dev/null -Ubad1 -- test/* | wc -l)
+standalone_test_directory=$test_directory_name/standalone
+standalone_test_directory_files=$standalone_test_directory/*
+integral_test_directory=$test_directory_name/integral
+integral_test_directory_files=$integral_test_directory/*
+standalone_test_count=$(ls 2>/dev/null -Ubad1 -- $standalone_test_directory/* | wc -l)
+integral_test_count=$(ls 2>/dev/null -Ubad1 -- $integral_test_directory/* | wc -l)
+total_test_count=$(($standalone_test_count + $integral_test_count))
+
 ran_test_count=0
 test_i=1
 passed_compilation_count=0
 failed_compilation_count=0
 failed_compilations=()
 failed_tests=()
+my_current_directory=$(pwd)
+nat_compiler=$my_current_directory/zig-out/bin/nat
 
-for dir in $test_directory
+for standalone_test_case in $standalone_test_directory_files
 do
-    MY_TESTNAME=${dir##*/}
-    zig build run -Duse_llvm=$nativity_use_llvm -- $dir/main.nat
+    STANDALONE_TEST_NAME=${standalone_test_case##*/}
+    $nat_compiler $standalone_test_case/main.nat
 
     if [[ "$?" == "0" ]]; then
         passed_compilation_count=$(($passed_compilation_count + 1))
         if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-            nat/$MY_TESTNAME
+            nat/$STANDALONE_TEST_NAME
+
+            if [[ "$?" == "0" ]]; then
+                passed_test_count=$(($passed_test_count + 1))
+                result="\e[32mPASSED\e[0m"
+            else
+                failed_test_count=$(($failed_test_count + 1))
+                result="\e[31mFAILED\e[0m"
+                failed_tests+=("$test_i. $STANDALONE_TEST_NAME")
+            fi
+
+            ran_test_count=$(($ran_test_count + 1))
+        else
+            result="\e[31mOS NOT SUPPORTED\e[0m"
+        fi
+    else
+        failed_compilation_count=$(($failed_compilation_count + 1))
+        result="\e[31mCOMPILATION FAILURE\e[0m"
+        failed_compilations+=("$test_i. $STANDALONE_TEST_NAME")
+    fi
+
+    echo -e "[$test_i/$total_test_count] [$result] $STANDALONE_TEST_NAME"
+
+    test_i=$(($test_i + 1))
+done
+
+for integral_test_case in $integral_test_directory_files
+do
+    MY_TESTNAME=${integral_test_case##*/}
+    cd test/integral/$MY_TESTNAME
+    $nat_compiler
+
+    if [[ "$?" == "0" ]]; then
+        passed_compilation_count=$(($passed_compilation_count + 1))
+        if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+            nat/src
 
             if [[ "$?" == "0" ]]; then
                 passed_test_count=$(($passed_test_count + 1))
@@ -47,6 +90,7 @@ do
     echo -e "[$test_i/$total_test_count] [$result] $MY_TESTNAME"
 
     test_i=$(($test_i + 1))
+    cd $my_current_directory
 done
 
 printf "\n"
