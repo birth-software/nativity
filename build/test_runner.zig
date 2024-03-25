328b9075069c116cabbe6b3d2e8e8db1c92ba07f
David Gonzalez Martin <davidgm94.work@protonmail.com>
2024-03-24 20:38:26 -0600

    Tiny C++ integration

--- build/test_runner.zig
+++ build/test_runner.zig
@@ -53,6 +53,7 @@ fn runStandalone(allocator: Allocator, args: struct {
             .allocator = allocator,
             // TODO: delete -main_source_file?
             .argv = &.{ "zig-out/bin/nat", if (args.is_test) "test" else "exe", "-main_source_file", source_file_path },
+            .max_output_bytes = std.math.maxInt(u64),
         });
         ran_compilation_count += 1;
 
@@ -82,6 +83,7 @@ fn runStandalone(allocator: Allocator, args: struct {
                 .allocator = allocator,
                 // TODO: delete -main_source_file?
                 .argv = &.{test_path},
+                .max_output_bytes = std.math.maxInt(u64),
             });
             ran_test_count += 1;
             const test_result: TestError!bool = switch (test_run.term) {
@@ -134,6 +136,7 @@ fn runStandaloneTests(allocator: Allocator) !void {
             .allocator = allocator,
             // TODO: delete -main_source_file?
             .argv = &.{ "zig-out/bin/nat", "exe", "-main_source_file", source_file_path },
+                .max_output_bytes = std.math.maxInt(u64),
         });
         ran_compilation_count += 1;
 
@@ -163,6 +166,7 @@ fn runStandaloneTests(allocator: Allocator) !void {
                 .allocator = allocator,
                 // TODO: delete -main_source_file?
                 .argv = &.{test_path},
+                .max_output_bytes = std.math.maxInt(u64),
             });
             ran_test_count += 1;
             const test_result: TestError!bool = switch (test_run.term) {
@@ -221,6 +225,7 @@ fn runBuildTests(allocator: Allocator) !void {
             .allocator = allocator,
             // TODO: delete -main_source_file?
             .argv = &.{ compiler_realpath, "build" },
+                .max_output_bytes = std.math.maxInt(u64),
         });
 
         ran_compilation_count += 1;
@@ -251,6 +256,7 @@ fn runBuildTests(allocator: Allocator) !void {
                 .allocator = allocator,
                 // TODO: delete -main_source_file?
                 .argv = &.{test_path},
+                .max_output_bytes = std.math.maxInt(u64),
             });
             ran_test_count += 1;
             const test_result: TestError!bool = switch (test_run.term) {
@@ -288,33 +294,14 @@ fn runBuildTests(allocator: Allocator) !void {
     }
 }
 
-pub fn main() !void {
+fn runStdTests(allocator: Allocator) !void {
     var errors = false;
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    const allocator = arena.allocator();
-    runStandalone(allocator, .{
-        .directory_path = "test/standalone",
-        .group_name = "STANDALONE",
-        .is_test = false,
-    }) catch {
-        errors = true;
-    };
-    runBuildTests(allocator) catch {
-        errors = true;
-    };
-    runStandalone(allocator, .{
-        .directory_path = "test/tests",
-        .group_name = "TEST EXECUTABLE",
-        .is_test = true,
-    }) catch {
-        errors = true;
-    };
-
     std.debug.print("std... ", .{});
 
     const result = try std.ChildProcess.run(.{
         .allocator = allocator,
         .argv = &.{ "zig-out/bin/nat", "test", "-main_source_file", "lib/std/std.nat", "-name", "std" },
+        .max_output_bytes = std.math.maxInt(u64),
     });
     const compilation_result: TestError!bool = switch (result.term) {
         .Exited => |exit_code| if (exit_code == 0) true else error.abnormal_exit_code,
@@ -341,6 +328,7 @@ pub fn main() !void {
             .allocator = allocator,
             // TODO: delete -main_source_file?
             .argv = &.{"nat/std"},
+            .max_output_bytes = std.math.maxInt(u64),
         });
         const test_result: TestError!bool = switch (test_run.term) {
             .Exited => |exit_code| if (exit_code == 0) true else error.abnormal_exit_code,
@@ -361,6 +349,171 @@ pub fn main() !void {
             std.debug.print("STDERR:\n\n{s}\n\n", .{test_run.stderr});
         }
     }
+
+    if (errors) return error.fail;
+}
+
+fn runCmakeTests(allocator: Allocator) !void {
+    var errors = false;
+    const original_dir = try std.fs.cwd().realpathAlloc(allocator, ".");
+    const cc_dir = try std.fs.cwd().openDir("test/cc", .{
+        .iterate = true,
+    });
+
+    const cc_dir_path = try cc_dir.realpathAlloc(allocator, ".");
+    try std.posix.chdir(cc_dir_path);
+
+    var cc_dir_iterator = cc_dir.iterate();
+    while (try cc_dir_iterator.next()) |cc_entry| {
+        switch (cc_entry.kind) {
+            .directory => {
+                std.debug.print("{s}...\n", .{cc_entry.name});
+                try std.posix.chdir(cc_entry.name);
+                try std.fs.cwd().deleteTree("build");
+                try std.fs.cwd().makeDir("build");
+                try std.posix.chdir("build");
+
+                const cmake = try std.ChildProcess.run(.{
+                    .allocator = allocator,
+                    // TODO: delete -main_source_file?
+                    .argv = &.{
+                        "cmake",
+                        "..",
+                        "-G", "Unix Makefiles",
+                        "-DCMAKE_VERBOSE_MAKEFILE=On",
+                        try std.mem.concat(allocator, u8, &.{ "-DCMAKE_C_COMPILER=", "nat;cc" }),
+                        try std.mem.concat(allocator, u8, &.{ "-DCMAKE_CXX_COMPILER=", "nat;c++" }),
+                        try std.mem.concat(allocator, u8, &.{ "-DCMAKE_ASM_COMPILER=", "nat;cc" }),
+                    },
+                    .max_output_bytes = std.math.maxInt(u64),
+                });
+                const cmake_result: TestError!bool = switch (cmake.term) {
+                    .Exited => |exit_code| if (exit_code == 0) true else error.abnormal_exit_code,
+                    .Signal => error.signaled,
+                    .Stopped => error.stopped,
+                    .Unknown => error.unknown,
+                };
+
+                const cmake_success = cmake_result catch b: {
+                    errors = true;
+                    break :b false;
+                };
+
+                if (!cmake_success) {
+                    if (cmake.stdout.len > 0) {
+                        std.debug.print("STDOUT:\n\n{s}\n\n", .{cmake.stdout});
+                    }
+                    if (cmake.stderr.len > 0) {
+                        std.debug.print("STDERR:\n\n{s}\n\n", .{cmake.stderr});
+                    }
+                }
+
+                var success = cmake_success;
+                if (success) {
+                    const ninja = try std.ChildProcess.run(.{
+                        .allocator = allocator,
+                        // TODO: delete -main_source_file?
+                        .argv = &.{
+                            "make"
+                        },
+                        .max_output_bytes = std.math.maxInt(u64),
+                        });
+                    const ninja_result: TestError!bool = switch (ninja.term) {
+                        .Exited => |exit_code| if (exit_code == 0) true else error.abnormal_exit_code,
+                        .Signal => error.signaled,
+                        .Stopped => error.stopped,
+                        .Unknown => error.unknown,
+                    };
+
+                    const ninja_success = ninja_result catch b: {
+                        errors = true;
+                        break :b false;
+                    };
+                    if (!ninja_success) {
+                        if (ninja.stdout.len > 0) {
+                            std.debug.print("STDOUT:\n\n{s}\n\n", .{ninja.stdout});
+                        }
+                        if (ninja.stderr.len > 0) {
+                            std.debug.print("STDERR:\n\n{s}\n\n", .{ninja.stderr});
+                        }
+                    }
+
+                    success = success and ninja_success;
+
+                    if (success) {
+                        const run = try std.ChildProcess.run(.{
+                            .allocator = allocator,
+                            // TODO: delete -main_source_file?
+                            .argv = &.{
+                                try std.mem.concat(allocator, u8, &.{ "./", cc_entry.name}),
+                            },
+                            .max_output_bytes = std.math.maxInt(u64),
+                        });
+                        const run_result: TestError!bool = switch (run.term) {
+                            .Exited => |exit_code| if (exit_code == 0) true else error.abnormal_exit_code,
+                            .Signal => error.signaled,
+                            .Stopped => error.stopped,
+                            .Unknown => error.unknown,
+                        };
+
+                        const run_success = run_result catch b: {
+                            errors = true;
+                            break :b false;
+                        };
+
+                        if (run.stdout.len > 0) {
+                            std.debug.print("STDOUT:\n\n{s}\n\n", .{run.stdout});
+                        }
+                        if (run.stderr.len > 0) {
+                            std.debug.print("STDERR:\n\n{s}\n\n", .{run.stderr});
+                        }
+
+                        success = success and run_success;
+                    }
+                }
+
+                std.debug.print("[TEST {s}]\n", .{if (success) "\x1b[32mOK\x1b[0m" else "\x1b[31mFAILED\x1b[0m"});
+            },
+            else => std.debug.panic("Entry {s} is a {s}", .{cc_entry.name, @tagName(cc_entry.kind)}),
+        }
+
+        try std.posix.chdir(cc_dir_path);
+    }
+
+    try std.posix.chdir(original_dir);
+
+    if (errors) {
+        return error.fail;
+    }
+}
+
+pub fn main() !void {
+    var errors = false;
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const allocator = arena.allocator();
+    runStandalone(allocator, .{
+        .directory_path = "test/standalone",
+        .group_name = "STANDALONE",
+        .is_test = false,
+    }) catch {
+        errors = true;
+    };
+    runBuildTests(allocator) catch {
+        errors = true;
+    };
+    runStandalone(allocator, .{
+        .directory_path = "test/tests",
+        .group_name = "TEST EXECUTABLE",
+        .is_test = true,
+    }) catch {
+        errors = true;
+    };
+
+    runStdTests(allocator) catch {
+        errors = true;
+    };
+
+    try runCmakeTests(allocator);
 
     if (errors) {
         return error.fail;
--- test/cc/c_first/main.c
+++ test/cc/c_first/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello world\n");
+    printf("Hello \n");
     return 0;
 }
