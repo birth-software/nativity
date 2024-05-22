3df904b3d3035ee8437a3f4dfeb420d361dade15
David Gonzalez Martin <davidgm94.work@protonmail.com>
2024-05-22 08:10:08 -0600

    Make script useful to test locally a single source

--- script.sh
+++ script.sh
@@ -2,11 +2,13 @@
 
 set -ex
 
+MY_SOURCE_FILE=retest/standalone/first/main.nat
+
 i=$((0))
 while ((i < 1000)); do
-    zig build run -Dprint_stack_trace=true -Doptimize=Debug -- exe -main_source_file retest/standalone/first/main.nat
-    zig build run -Dprint_stack_trace=true -Doptimize=ReleaseSafe -- exe -main_source_file retest/standalone/first/main.nat
-    zig build run -Dprint_stack_trace=true -Doptimize=ReleaseSmall -- exe -main_source_file retest/standalone/first/main.nat
-    zig build run -Dprint_stack_trace=true -Doptimize=ReleaseFast -- exe -main_source_file retest/standalone/first/main.nat
+    zig build run -Dprint_stack_trace=true -Doptimize=Debug -- exe -main_source_file $MY_SOURCE_FILE
+    zig build run -Dprint_stack_trace=true -Doptimize=ReleaseSafe -- exe -main_source_file $MY_SOURCE_FILE
+    zig build run -Dprint_stack_trace=true -Doptimize=ReleaseSmall -- exe -main_source_file $MY_SOURCE_FILE
+    zig build run -Dprint_stack_trace=true -Doptimize=ReleaseFast -- exe -main_source_file $MY_SOURCE_FILE
     i=$((i + 1))
 done
